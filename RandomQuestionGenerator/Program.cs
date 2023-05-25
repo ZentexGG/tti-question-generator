f79e212b367b520c36c39c5a5447bfa803684fee
ZentexGG <stefan.tanase2121@gmail.com>
2023-05-25 18:25:24 +0300

    add files

---
+++ RandomQuestionGenerator/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace RandomQuestionGenerator;
+
+public static class Program
+{
+    public static void PrintBeginning()
+    {
+        Console.WriteLine("Welcome! Please choose one of the following question categories to continue!\n" +
+                          "A. All\n" +
+                          "B. SQL Question\n" +
+                          "C. C# & .NET Question\n" +
+                          "D. Web Question");
+        Console.WriteLine("Type 'quit' if you wish to exit");
+    }
+    public static void Main(string[] args)
+    {
+        var questions = new QuestionStorage();
+        var generator = new QuestionGenerator(questions);
+        var userInput = string.Empty;
+
+        while (userInput != "quit")
+        {
+            PrintBeginning();
+            userInput = Console.ReadLine()?.ToLower();
+            switch (userInput)
+            {
+                case "a":
+                    Console.Clear();
+                    Console.WriteLine(generator.GetRandomQuestionFromAll());
+                    Console.WriteLine("Press ENTER to return to the main menu.");
+                    Console.ReadLine();
+                    break;
+                case "b":
+                    Console.Clear();
+                    Console.WriteLine(generator.GetRandomSql());
+                    Console.WriteLine("Press ENTER to return to the main menu.");
+                    Console.ReadLine();
+                    break;
+                case "c":
+                    Console.Clear();
+                    Console.WriteLine(generator.GetRandomDotNet());
+                    Console.WriteLine("Press ENTER to return to the main menu.");
+                    Console.ReadLine();
+                    break;
+                case "d":
+                    Console.Clear();
+                    Console.WriteLine(generator.GetRandomWeb());
+                    Console.WriteLine("Press ENTER to return to the main menu.");
+                    Console.ReadLine();
+                    break;
+                case "quit":
+                    return;
+                default:
+                    Console.WriteLine("Please enter a valid option!");
+                    break;
+            }
+        }
+    }
+}
---
+++ RandomQuestionGenerator/QuestionGenerator.cs
@@ -0,0 +1,41 @@
+﻿namespace RandomQuestionGenerator;
+
+public class QuestionGenerator
+{
+    private QuestionStorage Questions { get; set; }
+    private readonly Random _r = new Random();
+    public QuestionGenerator(QuestionStorage questions)
+    {
+        Questions = questions;
+    }
+
+    public string GetRandomQuestionFromAll()
+    {
+        var randomCategory = _r.Next(1, 4);
+        return randomCategory switch
+        {
+            1 => GetRandomWeb(),
+            2 => GetRandomDotNet(),
+            3 => GetRandomSql(),
+            _ => throw new IndexOutOfRangeException("The random category chosen does not exist!")
+        };
+    }
+
+    public string GetRandomSql()
+    {
+        Console.WriteLine("SQL Question");
+        return Questions.SQLQuestions[_r.Next(Questions.SQLQuestions.Length)];
+    }
+    
+    public string GetRandomWeb()
+    {
+        Console.WriteLine("Web Question");
+        return Questions.WebQuestions[_r.Next(Questions.WebQuestions.Length)];
+    }
+    
+    public string GetRandomDotNet()
+    {
+        Console.WriteLine("C# & .NET Question");
+        return Questions.CSharpDotNetQuestions[_r.Next(Questions.CSharpDotNetQuestions.Length)];
+    }
+}
